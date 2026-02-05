42bd8e2a5fca8881acfa6c3eff046ec68101e1aa
Pradipta Das <pradiptadas.1988@hotmail.com>
2026-02-06 00:20:32 +0530

    vpc and database-RDS created inside stack

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,10 +1,50 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
+    private Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+        this.vpc = createVpc();
+
+        DatabaseInstance authServiceDB =
+                createDatabaseInstance("AuthServiceDB", "auth-service-db");
+
+        DatabaseInstance patientServiceDB =
+                createDatabaseInstance("PatientServiceDB", "patient-service-db");
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder
+                .create(this, "PatientManagementVpc")
+                .vpcName("PatientManagementVpc")
+                .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabaseInstance(String id, String dbName) {
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                                PostgresInstanceEngineProps.builder()
+                                        .version(PostgresEngineVersion.VER_17_2)
+                                        .build()
+                        )
+                )
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
     }
 
     public static void main(final String[] args) {
